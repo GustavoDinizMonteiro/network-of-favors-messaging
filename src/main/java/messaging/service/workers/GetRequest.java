496d6977b1e757cc7849e7dddce14e53875d1fad
Gustavo Diniz Monteiro <dinizmonteirogustavo@gmail.com>
2019-03-17 21:25:49 -0300

    Add implementation to remote get order.

--- src/main/java/messaging/service/workers/GetRequest.java
+++ src/main/java/messaging/service/workers/GetRequest.java
@@ -1,21 +1,35 @@
 package messaging.service.workers;
 
-import org.dom4j.Element;
 import org.jamppa.component.handler.AbstractQueryHandler;
 import org.xmpp.packet.IQ;
 
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class GetRequest extends AbstractQueryHandler {
+	private static Dotenv dotenv = Dotenv.load();
 
 	public GetRequest() {
 		super("REMOTE_GET_ORDER");
 	}
 
 	public IQ handle(IQ iq) {
-		@SuppressWarnings("unused")
-		Element queryElement = iq.getElement().element("query")
-									.element("id");
-		// TODO: get order info from cloud provider
-		return null;
+		String orderId = iq.getElement()
+									.element("query")
+									.element("id")
+									.getText();
+		String endpoint = dotenv.get("XMPP_SERVER_IP");
+		IQ responseIQ = IQ.createResultIQ(iq);
+		try {
+			String response = Unirest.get(endpoint.concat(orderId))
+						.asJson().getBody().toString();
+			responseIQ.getElement().addElement("response", response);
+		} catch (UnirestException e) {
+			System.err.println(e.getMessage());
+		}
+		return responseIQ;
 	}
 
 }
